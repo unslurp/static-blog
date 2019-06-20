b2714b7aba48fb7df946de818f0de78dffeb3290
miklo <51862385+unslurp@users.noreply.github.com>
2019-06-20 20:52:55 +0200

    Update analytics.blade.php

--- source/_partials/analytics.blade.php
+++ source/_partials/analytics.blade.php
@@ -1,4 +1,3 @@
-<!-- analytics -->
 <script>
 /* eslint-disable */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -9,4 +8,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', '{{ $page->services->analytics }}', 'auto');
 ga('send', 'pageview');
 </script>
-<!-- end analytics -->
