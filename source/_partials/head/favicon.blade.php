4da63c5115c791ab283ed7ab279372dbd9d8aef8
miklo <51862385+unslurp@users.noreply.github.com>
2019-06-20 20:51:26 +0200

    added favicons

--- source/_partials/head/favicon.blade.php
+++ source/_partials/head/favicon.blade.php
@@ -1,5 +1,2 @@
-<!-- favicon -->
-{{-- try out https://realfavicongenerator.net --}}
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<!-- end favicon -->
+<link rel="icon" type="image/png" sizes="32x32" href="https://avatars1.githubusercontent.com/u/51862385">
+<link rel="icon" type="image/png" sizes="16x16" href="https://avatars1.githubusercontent.com/u/51862385">
