944669e520a28b28cf1a351c499cb39803d4267b
miklo <51862385+unslurp@users.noreply.github.com>
2019-06-20 20:49:57 +0200

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -5,7 +5,6 @@
 @section('content')
     <h1>{{ $page->title }}</h1>
 
-    {{-- I know inline CSS isn't good, but this is just a template so you should change everything anyway --}}
     @if ($page->image)
         <img src="{{ $page->imageCdn($page->image) }}" style="object-fit: cover; height: 250px; width: 100%;">
     @endif
@@ -24,17 +23,10 @@
 
     <hr>
 
-    <p>DISCLAIMER: Any 3rd-party services in these posts and in the config are only recommendations/suggestions. I am not affiliated with any of them.</p>
-
     @yield('postContent')
 
     <hr>
 
     @include('_partials.share')
 
-    @if ($page->comments)
-        @include('_partials.comments')
-    @else
-        <p>Comments are not enabled for this post.</p>
-    @endif
 @endsection
